4d25bfedce3f3bf800f55dd2e1ccc4d1117c0aff
Maliya-bro <darkmaliya10@gmail.com>
2026-01-04 14:42:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Ggpm1bgI#ia96pY5ZAOp_zWlTr5V4Sd-iLCv2fhyayf9boiXs6cw",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nadithmalindu-source/Malindu-AI-BOT/blob/main/image/Gemini_Generated_Image_unjbleunjbleunjb.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Maliya-bro/MALIYA-MD/blob/main/images/Gemini_Generated_Image_unjbleunjbleunjb.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALIYA-MD BOT Is Alive Now😍*",
 BOT_OWNER: '94701369636',  // Replace with the owner's phone number
 
