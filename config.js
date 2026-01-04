6284e520033d47a03e8cf25111eb8d8e765624a7
Maliya-bro <darkmaliya10@gmail.com>
2026-01-04 14:45:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Ggpm1bgI#ia96pY5ZAOp_zWlTr5V4Sd-iLCv2fhyayf9boiXs6cw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Maliya-bro/MALIYA-MD/blob/main/images/Gemini_Generated_Image_unjbleunjbleunjb.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALIYA-MD BOT Is Alive Now😍*",
-BOT_OWNER: '94701369636',  // Replace with the owner's phone number
+BOT_OWNER: '94771698234',  // Replace with the owner's phone number
 
 
 
