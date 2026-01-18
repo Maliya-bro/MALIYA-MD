16b8e858bd2ae8a60f87e38a3bdc7c258824d8ba
Maliya-bro <darkmaliya10@gmail.com>
2026-01-18 20:31:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "b4hjiSjT#4yh4QPvxrFpHqdCA3SgbrznReQpO-o29R98FszXs6t8",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Maliya-bro/MALIYA-MD/blob/main/images/a1b18d21-fd72-43cb-936b-5b9712fb9af0.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALIYA-MD BOT Is Alive Now😍*",
-BOT_OWNER: '94701369636',
+BOT_OWNER: '94702135392',
 AUTO_STATUS_SEEN: 'true',
 AUTO_STATUS_REACT: 'true',
 MODE: process.env.MODE || "public",
