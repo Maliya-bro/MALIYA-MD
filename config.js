02174a4d105eb4c1d0844065175b6c346f2bdf3c
Maliya-bro <darkmaliya10@gmail.com>
2026-02-07 19:19:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "mkwgxbSB#Y8mpjCfFkxu9aWuje1QZyKgxnaqm3M-f--d0VnkhLEY",
+SESSION_ID: process.env.SESSION_ID || "DgAm0bCb#EnUT4gIpQihmqufcURzDBo5QoIQhE3aI76Ll-af4KZw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Maliya-bro/MALIYA-MD/blob/main/images/WhatsApp%20Image%202026-01-18%20at%2012.37.23.jpeg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALIYA-MD Is Alive Now😍😍😍.*",
 BOT_OWNER: "94702135392",
