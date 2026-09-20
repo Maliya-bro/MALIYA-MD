a8f0eae811433aae69ae71d45db22a7496225bf1
Maliya-bro <darkmaliya10@gmail.com>
2026-09-20 13:14:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,8 +110,8 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization", "x-settings-token"]
 }));
 // ✅ පින්තූර (3MB දක්වා) Upload කරන්න ඉඩ දෙනවා
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(path.join(__dirname, "public")));
 
 const prefix         = ".";
